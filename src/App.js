6b569db56da2ed72818c2ffab7b0739a2cb1585f
HCJIN <ghkdckswls02@naver.com>
2023-05-02 10:22:32 +0900

    05.02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position)=>{
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log(lat,lon)
+    });
+  }
+  useEffect(()=>{
+    getCurrentLocation();
+  },[])
   return (
     <div >
       
     </div>
   );
-}
+} 
 
 export default App;
